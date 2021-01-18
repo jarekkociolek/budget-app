c95f2c721ab3dff2f708ce778e7dd1fa12ce20c1
jarekkociolek <jarekkociolek@gmail.com>
2021-01-18 21:09:36 +0100

    sign-in component without shared layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import "antd/dist/antd.css";
 import ".././index.css";
 import { Layout, Menu, Row, Avatar, Space, Popover } from "antd";
 import React, { useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ExpensesPage from "./expenses/ExpensesPage";
 import HomePage from "./home/HomePage";
 import AppSider from "./common/AppSider";
@@ -35,7 +35,7 @@ const App = (props) => {
       }
     }
     getUser();
-  }, []);
+  }, [props.user.isAuthenticated]);
 
   const [t] = useTranslation();
 
@@ -56,56 +56,63 @@ const App = (props) => {
 
   return (
     <>
-      <React.Suspense fallback="loading">
-        <Route path="/signin-oidc" component={SignInCallback}></Route>
-        <Layout style={{ height: "100vh" }}>
-          <Header>
-            <Row justify={"end"} align={"middle"}>
-              {props.user.isAuthenticated ? (
-                <>
-                  <Popover content={content}>
-                    <Avatar size="large" icon={<UserOutlined />} />{" "}
-                  </Popover>
-                  ,
-                </>
-              ) : (
-                <></>
-              )}
-              <Menu theme="dark" mode="horizontal">
-                {!props.user.isAuthenticated ? (
-                  <>
-                    <Menu.Item key="1" onClick={handleLogin}>
-                      {t("menu_login")}
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                      <a href={oidcConfig.registration_uri}>
-                        {t("menu_register")}
-                      </a>
-                    </Menu.Item>
-                  </>
-                ) : (
-                  <>
-                    <Menu.Item key="4" onClick={handleLogout}>
-                      {t("menu_logout")}
-                    </Menu.Item>
-                  </>
-                )}
-              </Menu>
-            </Row>
-          </Header>
-          <Layout>
-            {props.user.isAuthenticated ? <AppSider></AppSider> : <></>}
-            <Content>
-              <Switch>
-                <Route exact path="/" component={HomePage}></Route>
-                <Route path="/expenses" component={ExpensesPage}></Route>
-                <Route component={PageNotFound}></Route>
-              </Switch>
-              <ToastContainer autoClose={3000} hideProgressBar></ToastContainer>
-            </Content>
-          </Layout>
-        </Layout>
-      </React.Suspense>
+      <Router>
+        <Switch>
+          <Route path="/signin-oidc" component={SignInCallback}></Route>
+          <Route path="/">
+            <Layout style={{ height: "100vh" }}>
+              <Header>
+                <Row justify={"end"} align={"middle"}>
+                  {props.user.isAuthenticated ? (
+                    <>
+                      <Popover content={content}>
+                        <Avatar size="large" icon={<UserOutlined />} />{" "}
+                      </Popover>
+                      ,
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                  <Menu theme="dark" mode="horizontal">
+                    {!props.user.isAuthenticated ? (
+                      <>
+                        <Menu.Item key="1" onClick={handleLogin}>
+                          {t("menu_login")}
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                          <a href={oidcConfig.registration_uri}>
+                            {t("menu_register")}
+                          </a>
+                        </Menu.Item>
+                      </>
+                    ) : (
+                      <>
+                        <Menu.Item key="4" onClick={handleLogout}>
+                          {t("menu_logout")}
+                        </Menu.Item>
+                      </>
+                    )}
+                  </Menu>
+                </Row>
+              </Header>
+              <Layout>
+                {props.user.isAuthenticated ? <AppSider></AppSider> : <></>}
+                <Content>
+                  <Switch>
+                    <Route exact path="/" component={HomePage}></Route>
+                    <Route path="/expenses" component={ExpensesPage}></Route>
+                    <Route component={PageNotFound}></Route>
+                  </Switch>
+                  <ToastContainer
+                    autoClose={3000}
+                    hideProgressBar
+                  ></ToastContainer>
+                </Content>
+              </Layout>
+            </Layout>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
--- src/components/SignInCallback.js
+++ src/components/SignInCallback.js
@@ -5,17 +5,24 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as authenticationActions from "../redux/actions/authenticationActions";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 
-const SignInCallback = () => {
+const SignInCallback = (props) => {
+  const [t] = useTranslation();
+
   const history = useHistory();
   useEffect(() => {
     async function signinAsync() {
       await userService.signinRedirectCallback();
+      let usr = {
+        isAuthenticated: true,
+      };
+      props.actions.setUserAuthenticated(usr);
       history.push("/");
     }
     signinAsync();
   }, [history]);
-  return <h1>You are being logged in...</h1>;
+  return <h1>{t("logging_in_info")}</h1>;
 };
 
 SignInCallback.propTypes = {
